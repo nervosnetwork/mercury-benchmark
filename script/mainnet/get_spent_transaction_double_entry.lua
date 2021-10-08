2295387647d44e5029c1ec7df37bf596e11280c6
fjchen7 <fjchen7@gmail.com>
2021-10-08 19:02:01 +0800

    feat: add response check in lua script

--- script/mainnet/get_spent_transaction_double_entry.lua
+++ script/mainnet/get_spent_transaction_double_entry.lua
@@ -1,3 +1,9 @@
 wrk.method = "POST"
-wrk.body   = "{\"jsonrpc\":\"2.0\",\"method\":\"get_spent_transaction\",\"params\":[{\"outpoint\":{\"tx_hash\":\"baea5058401c63e1eaaf33ef9155afaf6c57be038e8599f1d4e59fb24b63e512\",\"index\":\"0\"},\"view_type\":\"TransactionView\"}],\"id\":100}"
+wrk.body   = "{\"jsonrpc\":\"2.0\",\"method\":\"get_spent_transaction\",\"params\":[{\"outpoint\":{\"tx_hash\":\"0xf004b16641c2a7418e075250e957bdc31234cf2ff9fed236faf17d25f79222f9\",\"index\":\"0x0\"},\"structure_type\":\"DoubleEntry\"}],\"id\":100}"
 wrk.headers["Content-Type"] = "application/json"
+
+function response(status, headers, body)
+    if (string.find(body, '"error"')) then
+        print('error, resp: ', body)
+    end
+end
--- script/mainnet/get_spent_transaction_native.lua
+++ script/mainnet/get_spent_transaction_native.lua
@@ -1,3 +1,9 @@
 wrk.method = "POST"
-wrk.body   = "{\"jsonrpc\":\"2.0\",\"method\":\"get_spent_transaction\",\"params\":[{\"outpoint\":{\"tx_hash\":\"0x6ed4024a1958c716b03b263cbfdeba1bfa70de673e5186a4f9bd5372641171c3\",\"index\":\"0x1\"},\"structure_type\":\"DoubleEntry\"}],\"id\":100}"
+wrk.body   = "{\"jsonrpc\":\"2.0\",\"method\":\"get_spent_transaction\",\"params\":[{\"outpoint\":{\"tx_hash\":\"0xf004b16641c2a7418e075250e957bdc31234cf2ff9fed236faf17d25f79222f9\",\"index\":\"0x0\"},\"structure_type\":\"Native\"}],\"id\":100}"
 wrk.headers["Content-Type"] = "application/json"
+
+function response(status, headers, body)
+    if (string.find(body, '"error"')) then
+        print('error, resp: ', body)
+    end
+end
