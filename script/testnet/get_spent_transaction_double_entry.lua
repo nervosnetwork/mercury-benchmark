64d262b0a946269c7f30b7bf76ca3ec8ebee76de
“zhengjianhui” <815168733@qq.com>
2021-10-14 01:40:17 +0800

    fix(script): Script errors

--- script/testnet/get_spent_transaction_double_entry.lua
+++ script/testnet/get_spent_transaction_double_entry.lua
@@ -1,3 +1,10 @@
 wrk.method = "POST"
-wrk.body   = "{\"jsonrpc\":\"2.0\",\"method\":\"get_spent_transaction\",\"params\":[{\"outpoint\":{\"tx_hash\":\"baea5058401c63e1eaaf33ef9155afaf6c57be038e8599f1d4e59fb24b63e512\",\"index\":\"0\"},\"view_type\":\"TransactionView\"}],\"id\":100}"
+wrk.body   = "{\"jsonrpc\":\"2.0\",\"method\":\"get_spent_transaction\",\"params\":[{\"outpoint\":{\"tx_hash\":\"0xb2e952a30656b68044e1d5eed69f1967347248967785449260e3942443cbeece\",\"index\":\"0x1\"},\"structure_type\":\"DoubleEntry\"}],\"id\":100}"
 wrk.headers["Content-Type"] = "application/json"
+
+function response(status, headers, body)
+    if (string.find(body, '"error"')) then
+        print('error, resp: ', body)
+        wrk.thread:stop()
+    end
+end
--- tests/wrk_test.py
+++ tests/wrk_test.py
@@ -9,7 +9,7 @@ class WrkTest(unittest.TestCase):
     def setUp(self):
         config = WrkConfig(node_url="http://127.0.0.1:8116", seconds=[10], collections=[300], threads=16, timeout=1, script_dir="script/testnet")
         self.config = config
-        scripts = ["build_smart_transfer_transaction.lua", "build_withdraw_transaction.lua"]
+        scripts = ["get_spent_transaction_double_entry.lua"]
         self.scripts = scripts
 
     def tearDown(self):
